501a2520a063464927e9f058fabdb9f9c8f2574b
Evgenii Zaev <johnnyzaev@gmail.com>
2026-06-28 11:47:02 +0500

    Added eslint, prettier and husky

---
+++ video-processing-service/eslint.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+
+export default defineConfig({
+  files: ['**/*.{js,ts}'],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    prettierConfig,
+  ],
+});
--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,4 +1,4 @@
-import express, {Express} from 'express';
+import express, { Express } from 'express';
 import ffmpeg from 'fluent-ffmpeg';
 
 const app: Express = express();
@@ -6,25 +6,27 @@ app.use(express.json());
 const port: string = process.env.PORT || '3000';
 
 app.post('/process-video', (req, res) => {
-    const inputFilePath= req.body.inputFilePath;
-    const outputFilePath = req.body.outputFilePath;
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
 
-    if (!inputFilePath || !outputFilePath) {
-        return res.status(400).send('Bad request: Input and output file paths are required');
-    }
+  if (!inputFilePath || !outputFilePath) {
+    return res
+      .status(400)
+      .send('Bad request: Input and output file paths are required');
+  }
 
-    ffmpeg(inputFilePath)
-        .outputOptions('-vf', 'scale=-1:360')
-        .on('end', () => {
-            return res.status(200).send('Processing finished successfully.');
-        })
-        .on('error', (err) => {
-            console.error(err);
-            res.status(500).send(`Internal Server Error: ${err.message}`);
-        })
-        .save(outputFilePath);
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360')
+    .on('end', () => {
+      return res.status(200).send('Processing finished successfully.');
+    })
+    .on('error', (err) => {
+      console.error(err);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+  console.log(`Listening on port ${port}`);
+});
